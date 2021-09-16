c4b22b860ff886f2c3f84a14166468c844da6e5a
chaolitan <ctan7749@gmail.com>
2021-09-16 10:20:52 -0700

    refactor: update style

--- src/danmaku/DanmakuRollItem.tsx
+++ src/danmaku/DanmakuRollItem.tsx
@@ -15,67 +15,70 @@ import { DanmakuRollItemProps } from './types'
 
 
 function DanmakuRollItem(props: DanmakuRollItemProps) {
+  const rollAnim = useRef(new Animated.Value(0)).current;
 
-    const rollAnim = useRef(new Animated.Value(0)).current
+  const rollValue = useRef(0);
+  const [rollToValue, setRollToValue] = useState(0);
 
-    const rollValue = useRef(0)
-    const [rollToValue, setRollToValue] = useState(0)
+  const init = (event: LayoutChangeEvent) => {
+    let width = event.nativeEvent.layout.width;
+    let toValue = -props.containerWidth - width;
+    setRollToValue(toValue);
 
-    const init = (event: LayoutChangeEvent) => {
-        let width = event.nativeEvent.layout.width
-        let toValue = - props.containerWidth - width
-        setRollToValue(toValue)
-
-        rollAnim.removeAllListeners()
-        rollAnim.addListener(({ value }) => {
-            props.setRight(Math.abs(value) > width)
-            if (value == toValue) props.onEnd()
-        })
-
-    }
-    useEffect(() => {
-        if (!rollToValue) return
-        rollAnim.stopAnimation((value) => {
-            rollValue.current = value
-        })
-        if (!props.playState) return
-        Animated.timing(
-            rollAnim,
-            {
-                toValue: rollToValue,
-                duration: (rollValue.current - rollToValue) / (props.speed * props.speed),
-                easing: Easing.linear,
-                useNativeDriver: true
-            }
-        ).start()
-    }, [props.playState, rollToValue, props.speed])
-    return (
-        <Animated.Text
-            onLayout={init}
-            numberOfLines={1}
-            style={{
-                position: 'absolute',
-                top: props.initVertical,
-                left: props.initLeft,
-                transform: [
-                    {
-                        translateX: rollAnim
-                    }
-                ],
-                color: props.color ? props.color : '#fff',
-                fontSize: props.fontSize,
-                fontWeight: 'bold',
-                textShadowRadius: props.shadowHidden ? undefined : 1,
-                textShadowColor: props.shadowHidden ? undefined : `rgba(0,0,0,${props.opacity})`,
-                opacity: props.opacity,
-                borderWidth: props.mark ? 1 : 0,
-                borderColor: '#777',
-                paddingHorizontal: 2
-            }}
-        >
-            {props.msg}
-        </Animated.Text>
-    )
+    rollAnim.removeAllListeners();
+    rollAnim.addListener(({ value }) => {
+      props.setRight(Math.abs(value) > width);
+      if (value == toValue) props.onEnd();
+    });
+  };
+  useEffect(() => {
+    if (!rollToValue) return;
+    rollAnim.stopAnimation((value) => {
+      rollValue.current = value;
+    });
+    if (!props.playState) return;
+    Animated.timing(rollAnim, {
+      toValue: rollToValue,
+      duration: (rollValue.current - rollToValue) / (props.speed * props.speed),
+      easing: Easing.linear,
+      useNativeDriver: true,
+    }).start();
+  }, [props.playState, rollToValue, props.speed]);
+  return (
+    <Animated.View
+      onLayout={init}
+      style={{
+        position: "absolute",
+        top: props.initVertical,
+        left: props.initLeft,
+        transform: [
+          {
+            translateX: rollAnim,
+          },
+        ],
+        height: 24,
+        borderWidth: props.mark ? 1 : 0,
+        borderColor: "#777",
+        borderRadius: 15,
+        backgroundColor: props.color ? props.color : "rgba(0,0,0,0.3)",
+        justifyContent: "center",
+      }}
+    >
+      <Animated.Text
+        style={{
+          color: "#fff",
+          fontSize: 14 || props.fontSize,
+          fontWeight: "500",
+          textShadowRadius: props.shadowHidden ? undefined : 1,
+          textShadowColor: props.shadowHidden
+            ? undefined
+            : `rgba(0,0,0,${props.opacity})`,
+          opacity: props.opacity,
+        }}
+      >
+        {props.msg}
+      </Animated.Text>
+    </Animated.View>
+  );
 }
-
-export default memo(DanmakuRollItem)
+export default memo(DanmakuRollItem);
