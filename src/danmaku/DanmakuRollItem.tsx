38d9ff760c55d18629b2e493842fa8fa42739e9b
jankergg <jankergg@gmail.com>
2021-10-01 15:12:09 -0700

    add pading into RollItem

--- src/danmaku/DanmakuRollItem.tsx
+++ src/danmaku/DanmakuRollItem.tsx
@@ -1,18 +1,8 @@
-import * as React from 'react'
-import {
-    useEffect,
-    useRef,
-    useState,
-    memo
-} from 'react'
-import {
-    Animated,
-    LayoutChangeEvent,
-    Easing
-} from 'react-native'
+import * as React from 'react';
+import {useEffect, useRef, useState, memo} from 'react';
+import {Animated, LayoutChangeEvent, Easing} from 'react-native';
 
-import { DanmakuRollItemProps } from './types'
-
+import {DanmakuRollItemProps} from './types';
 
 function DanmakuRollItem(props: DanmakuRollItemProps) {
   const rollAnim = useRef(new Animated.Value(0)).current;
@@ -26,14 +16,14 @@ function DanmakuRollItem(props: DanmakuRollItemProps) {
     setRollToValue(toValue);
 
     rollAnim.removeAllListeners();
-    rollAnim.addListener(({ value }) => {
+    rollAnim.addListener(({value}) => {
       props.setRight(Math.abs(value) > width);
       if (value == toValue) props.onEnd();
     });
   };
   useEffect(() => {
     if (!rollToValue) return;
-    rollAnim.stopAnimation((value) => {
+    rollAnim.stopAnimation(value => {
       rollValue.current = value;
     });
     if (!props.playState) return;
@@ -48,7 +38,7 @@ function DanmakuRollItem(props: DanmakuRollItemProps) {
     <Animated.View
       onLayout={init}
       style={{
-        position: "absolute",
+        position: 'absolute',
         top: props.initVertical,
         left: props.initLeft,
         transform: [
@@ -58,24 +48,24 @@ function DanmakuRollItem(props: DanmakuRollItemProps) {
         ],
         height: 24,
         borderWidth: props.mark ? 1 : 0,
-        borderColor: "#777",
+        borderColor: '#777',
         borderRadius: 15,
-        backgroundColor: props.color ? props.color : "rgba(0,0,0,0.3)",
-        justifyContent: "center",
-      }}
-    >
+        paddingLeft: 15,
+        paddingRight: 15,
+        backgroundColor: props.color ? props.color : 'rgba(0,0,0,0.3)',
+        justifyContent: 'center',
+      }}>
       <Animated.Text
         style={{
-          color: "#fff",
+          color: '#fff',
           fontSize: 14 || props.fontSize,
-          fontWeight: "500",
+          fontWeight: '500',
           textShadowRadius: props.shadowHidden ? undefined : 1,
           textShadowColor: props.shadowHidden
             ? undefined
             : `rgba(0,0,0,${props.opacity})`,
           opacity: props.opacity,
-        }}
-      >
+        }}>
         {props.msg}
       </Animated.Text>
     </Animated.View>
